bd06514f6ec17d5eb9c8bbb6e32e2fed2f8f48ca
masum0191 <masumidb2018@gmail.com>
2024-05-07 08:32:51 +0600

    route blade controller add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
-
+  
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+  
+use App\Http\Controllers\EventController;
+  
+Route::controller(EventController::class)->group(function(){
+    Route::get('full-calender', 'index');
+    Route::post('full-calender-ajax', 'ajax');
 });
